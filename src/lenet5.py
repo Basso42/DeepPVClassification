ce3798de6eb88c413c1bae179bf3b479adb2e4af
y_r <yanis.remmache@gmail.com>
2023-03-26 21:28:05 +0200

    Ajout data augmentation lenet5

--- src/lenet5.py
+++ src/lenet5.py
@@ -1,73 +1,25 @@
-import torch.nn as nn
+import keras.layers as layers
+import keras
+import tensorflow as tf
+from src.metrics import recall_m, precision_m, f1_m
 
-#n_classes = dimension de l'espace de Y (2 comme on est en classification binaire)
-class LeNet5(nn.Module):
+def buildModel(learnRate=0.01, dropout=0.2):
+    model = keras.Sequential()
+    # Layer 1 Conv2D
+    model.add(layers.Conv2D(filters=6, kernel_size=(5, 5), strides=(1, 1), activation='tanh', input_shape=(28,28,3), padding="same"))
+    # Layer 2 Pooling Layer
+    model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+    # Layer 3 Conv2D
+    model.add(layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(1, 1), activation='tanh', padding='valid'))
+    # Layer 4 Pooling Layer
+    model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(units=120, activation='tanh'))
+    model.add(layers.Dense(units=84, activation='tanh'))
+    model.add(layers.Dense(units=1, activation='sigmoid'))
+    model.compile(optimizer='sgd',loss=tf.keras.losses.binary_crossentropy,metrics=["Accuracy", recall_m, precision_m, f1_m])
 
-    def __init__(self, n_classes):
-        super(LeNet5, self).__init__()
 
-        # Première partie du modèle, qui correspond aux couches convolutionnelles.
-        #Remarque: padding: ajoute les 0 artificiels sur les bords du tenseur
-        #padding = 1 400*400 -> 402*402
-        #padding = 2 400*400 -> 404*404
+    #model.compile(loss=loss_function_used, optimizer=tf.keras.optimizers.SGD(learning_rate=0.01), metrics=[keras.metrics.Accuracy(name="Accuracy"),f1_m ,keras.metrics.Precision(name="Precision"), keras.metrics.Recall(name="Recall")])
 
-        self.feature_extractor = nn.Sequential(
-            ####Première couche de convolution####
-
-            # 3 composantes par pixel (si noir et blanc: 1): in_channels = 3
-            # 6 filtres: out_channels=6
-            # Dimension du motif = 5
-            # Stride (pas) = 1
-            # Padding = 2
-            # Activation tanh
-            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding = 2),
-            nn.Tanh(),
-
-            #Première couche de pooling
-            nn.AvgPool2d(kernel_size=2, stride=2),
-
-            #Résumé
-
-            #Input: 28*28*3
-            #Après padding: 32*32*3
-            #Après convolution: 28*28*6 (400=32-5+1)
-            #Après pooling: 14*14*6
-
-        ####Deuxième couche de convolution####
-
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
-            nn.Tanh(),
-
-            #Deuxième couche de pooling
-
-            nn.AvgPool2d(kernel_size=2, stride=2),
-
-            #Résumé
-
-            #Input: 14*14*6
-            #Après convolution: 10*10*16 (10=14-5+1)
-            #Après pooling: 5*5*16
-        
-        ####Troisième couche de convolution####
-
-            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
-            nn.Tanh(),
-
-            #Input: 5*5*16
-            #Après convolution: 1*1*120 (1=5-5+1)
-
-            
-            nn.Flatten(start_dim = 1, end_dim = -1), # on applique un applatissement à la dernière couche pour renvoyer un vecteur de taille 94*94*120=1060320 au classifieur
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(in_features=120, out_features=84),
-            nn.Tanh(),
-            nn.Linear(in_features=84, out_features=n_classes),
-        )
-
-    def forward(self, x):
-        x = self.feature_extractor(x)
-        logits = self.classifier(x)
-        # probs = F.softmax(logits, dim=1)
-        return logits# , probs
+    return model
