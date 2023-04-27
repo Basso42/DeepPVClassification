8c8bccdb64bc789f5e555d52c800d0f38fa3ce93
y_r <yanis.remmache@gmail.com>
2023-04-27 14:08:59 +0200

    delete NA

--- src/dataloader.py
+++ src/dataloader.py
@@ -20,26 +20,18 @@ warnings.filterwarnings("ignore")
 plt.ion() 
 class LabelAttribution:
 
-    def __init__(self, path_image_google, path_mask_google, path_image_ign, path_mask_ign,
+    def __init__(self, path_image_google, path_mask_google,
                  path_metadata,
                  colonne_identifiant,
                  path_export_train_test,
-                 use_img_google=True,
-                 use_img_ign=False
                  ):
 
         self.path_image_google=path_image_google
         self.path_mask_google=path_mask_google
 
-        self.path_image_ign=path_image_ign
-        self.path_mask_ign=path_mask_ign
-
         self.path_metadata=path_metadata
         self.colonne_identifiant=colonne_identifiant
         self.path_export_train_test=path_export_train_test
-
-        self.use_img_google=use_img_google
-        self.use_img_ign=use_img_ign
     
     def run(self):
 
@@ -51,62 +43,32 @@ class LabelAttribution:
             dirs = os.listdir(path) #on définit le directory d'où on souhaite extraire le nom des fichiers
             return [file.replace('.png','') for file in dirs]
         
-        # On récupère tous les noms des images Google et IGN
-
-        images = f(self.path_image_google) + f(self.path_image_ign)
+        # On récupère tous les noms des images Google
 
         im_google = f(self.path_image_google)
-        im_ign = f(self.path_image_ign)
 
         im_mask_google = f(self.path_mask_google)
-        im_mask_ign = f(self.path_mask_ign)
 
         #### Conversion en DF
-        im = pd.DataFrame (images, columns = [self.colonne_identifiant])
 
         im_google = pd.DataFrame (im_google, columns = [self.colonne_identifiant])
-        im_ign = pd.DataFrame (im_ign, columns = [self.colonne_identifiant])
 
         im_mask_google = pd.DataFrame (im_mask_google, columns = [self.colonne_identifiant])
-        im_mask_ign = pd.DataFrame (im_mask_ign, columns = [self.colonne_identifiant])
-
-        #On label '1' toutes les images comportant un masque et 0 sinon
-        im['Label'] = im[self.colonne_identifiant].isin(im_mask_ign[self.colonne_identifiant].append(im_mask_google[self.colonne_identifiant])).astype(int)
 
         #On label '1' toutes les images Google et '0' sinon
-        im['L_Google'] = im[self.colonne_identifiant].isin(im_google[self.colonne_identifiant]).astype(int)
-
-        #On label '1' toutes les images IGN et '0' sinon
-        im['L_IGN'] = im[self.colonne_identifiant].isin(im_ign[self.colonne_identifiant]).astype(int)
+        im_google['Label'] = im_google[self.colonne_identifiant].isin(im_mask_google[self.colonne_identifiant]).astype(int)
 
 
-        if (self.use_img_ign==False)&(self.use_img_google==True):
-        #On ne garde que les images Google
-               im = im.drop(im[im.L_Google<1].index) #on supprime les images IGN sans équivalent Google
+        im_google = im_google.drop_duplicates()
+   
+        # #On ne garde que l'identifiant et les labels
 
+        im_google= im_google[[self.colonne_identifiant, 'Label']]
+        im_google[self.colonne_identifiant] = im_google[self.colonne_identifiant].astype(str) + '.png' #on rajoute les extensions pour que le dataloader récupère les fichiers
 
-        
-        elif (self.use_img_ign==True)&(self.use_img_google==False):
-        #On ne garde que les images IGN
-                im = im.drop(im[im.L_IGN<1].index) #on supprime les images Google sans équivalent IGN
+        #On sépare le dataset en un training dataset et un test dataset avec sklearn
 
-        #Sinon garde toutes les images
-
-        #On sait que beaucoup d'images Google ont leur équivalent IGN (échelle différente) 
-        #On ne garde donc qu'un seul nom (comme sur le fichier metadata)
-
-        im = im.drop_duplicates()
-
-        df = pd.merge(annotations_file, im, how="outer", on=[self.colonne_identifiant]) #cette partie de script ne récupère que les noms déjà présents dans la base
-
-        #On ne garde que l'identifiant et les labels
-
-        df= df[[self.colonne_identifiant, 'Label']]
-        df[self.colonne_identifiant] = df[self.colonne_identifiant].astype(str) + '.png' #on rajoute les extensions pour que le dataloader récupère les fichiers
-
-        #On sépare le dataset en un training dataset et un test dataset avec sci-kit learn
-
-        train_df, test_df = train_test_split(df, test_size=0.2, random_state=0)
+        train_df, test_df = train_test_split(im_google, test_size=0.2, random_state=0)
 
         train_df.to_csv(self.path_export_train_test+'/'+'train_data.csv', index=False)
         test_df.to_csv(self.path_export_train_test+'/'+'test_data.csv', index=False)
